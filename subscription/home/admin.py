5078d9a7f155e452100aa38552822e09ac141084
rpmech03 <101850564+rpmech03@users.noreply.github.com>
2023-02-09 12:13:46 +0530

    custom middleware

--- subscription/home/admin.py
+++ subscription/home/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
-from .models import *
+from . import models
 
-admin.site.register(Blog)
+admin.site.register(models.Blog)
+admin.site.register(models.Subscription)
+admin.site.register(models.SubscriptionOrder)
--- subscription/middlewares/middleware.py
+++ subscription/middlewares/middleware.py
@@ -1,4 +1,8 @@
 from home.models import Subscription
+from rest_framework.response import Response
+from django.http import JsonResponse
+from home.models import SubscriptionOrder
+import datetime
 
 class SubscriptionCheck(object):
     def __init__(self,get_response) -> None:
@@ -7,9 +11,31 @@ class SubscriptionCheck(object):
     def __call__(self, request): #here we apply all logic
         # print(request.path) #route ko check krege ki actual m kaunsa route hai
         url_path = set((request.path).split('/'))
-        if 'blog_detail' in url_path:
+        if 'blog_detail' in url_path :
                 # print('yes found')
                 if not request.user.is_authenticated:
-                    
+                    return JsonResponse({
+                        'status' : False,
+                        'message' : 'you are not authenticated',
+                        'data' : {}
+                    },
+                    content_type = 'application/json'
+                    )
+
+                else:
+                    subscription_order = SubscriptionOrder.objects.filter (
+                        user = request.user,
+                        is_paid = True,
+                        subscription_end_date__gte = datetime.datetime.today().date() #current date se jyada ki date chahye ho tb
+                    )
+
+                if not subscription_order.exists():
+                    return JsonResponse({
+                        'status' : False,
+                        'message' : 'you havent purchased the subscription',
+                        'data' : {}
+                    },
+                    content_type = 'application/json'
+                    )
         return self.get_response(request)
         
